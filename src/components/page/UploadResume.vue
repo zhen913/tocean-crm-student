4bbbd3649af0d0deb174470cd14bc4f71abd936c
xzhen <1019921546@qq.com>
2017-11-06 14:35:12 +0800

    完成了上传简历页面

--- src/components/page/UploadResume.vue
+++ src/components/page/UploadResume.vue
@@ -10,21 +10,30 @@
         <div class="plugins-tips" style="text-align: right">
             <input type="button" value="提交" style="color: #1A438E"/>
         </div>
-
-        <div class="plugins-tips">
+        <div class="title">
             <div style="margin-top: 0;padding-top: 0;">
                 <el-button @click="show3 = !show3" class="button" style="margin-right: auto">
                     <el-breadcrumb separator=" ">
-                        <el-breadcrumb-item style="color:#1A438E;font-size:0.8em">基本资料</el-breadcrumb-item>
+                        <el-breadcrumb-item style="color:#D1E2F2;font-size:0.8em">基本资料</el-breadcrumb-item>
                         <el-breadcrumb-item>（+点击标题栏展开）</el-breadcrumb-item>
                     </el-breadcrumb>
                 </el-button>
                 <el-collapse-transition>
                     <div v-show="show3" style="margin:5px 15px;">
                         <div class="transition-box">
-                            <el-form :inline="true">
-                            简历：<input type="text"/><input type="button" value="预览"/>
-                            </el-form>
+                               <div class="bg-purple" >简历:</div>
+                               <div style="flex-grow: 1;">
+                                <el-row :gutter="20">
+                                    <el-col :span="18">
+                                    <div class="grid-content bg-purple-light" style="flex-grow: 1;margin-right: 0;">
+                                    <el-input placeholder="请输入内容" prefix-icon="el-icon-search" v-model="input21">
+                                    </el-input>
+                                </div></el-col>
+                                <el-col :span="2"><div class="grid-content row-bg">
+                                    <input type="button" value="预览..."/>
+                                </div></el-col>
+                            </el-row>
+                            </div>
                         </div>
                     </div>
                 </el-collapse-transition>
@@ -44,22 +53,46 @@
     }
 </script>
 <style>
+    .title{
+        border:1px  solid #B3CDE8;
+        margin-bottom: 20px;
+    }
     .transition-box {
-        margin-bottom: 10px;
-        width: 200px;
-        height: 100px;
-        border-radius: 4px;
-        background-color:#D6E5F4;
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        border:1px  solid #B3CDE8;
         text-align: center;
-        color: #fff;
-        padding: 40px 20px;
-        box-sizing: border-box;
-        margin-right: 20px;
+        background:#FFFFFF;
+        color:#1A438E;
     }
     .button{
         width: 100%;
+        margin-top: auto;
         background-color: #D6E5F4;
-
     }
+    .bg-purple {
+        width: 10%;
+        border-right:1px  solid #B3CDE8;
+        padding-right: 10px;
+        padding-top: 10px;
+
+        text-align: right;
+        background: #EEF7FD;
+    }
+    .bg-purple-light {
+        background: #e5e9f2;
+        margin: 5px;
+        margin-left: 10px;
+        margin-left: 0;
+    }
+    .grid-content {
+        min-height: 36px;
+    }
+    .row-bg {
+        margin-top: 10px;
+        text-align: right;
+    }
+
 </style>
 
