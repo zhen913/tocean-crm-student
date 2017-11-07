b896b376ac4f48e6541cabde664ec36c2bd2f8a3
xzhen <1019921546@qq.com>
2017-11-07 11:00:56 +0800

    完成提交报告页面

--- src/components/page/SubmitReport.vue
+++ src/components/page/SubmitReport.vue
@@ -2,7 +2,10 @@
 
     <div>
         <div class="plugins-tips">
-                <el-breadcrumb>当前功能：<b>调查列表管理</b></el-breadcrumb>
+            <el-breadcrumb separator="：">
+                <el-breadcrumb-item><i class="el-icon-date"></i> 当前功能</el-breadcrumb-item>
+                <el-breadcrumb-item style="color:#1A438E;font-size:0.8em">调查列表管理</el-breadcrumb-item>
+            </el-breadcrumb>
         </div>
         <div class="plugins-tips" style="margin-top: 0;padding-top: 0;">
             <el-button @click="show11 = !show11" class="button" style="margin-left: 0;margin-right: 0;">
@@ -11,18 +14,15 @@
                     <el-breadcrumb-item>(+单击标题栏展开)</el-breadcrumb-item>
                 </el-breadcrumb>
             </el-button>
-
-            <el-main v-show="show11">
-                <table>
-                    <tr>
-                        <th>序号</th>
-                        <th>调查名称</th>
-                        <th>调查内容</th>
-                        <th>考评对象</th>
-                        <th>调查时间</th>
-                    </tr>
-                </table>
-            </el-main>
+            <div v-show="show11" style="margin-top: 10px;color:#20a0ff">
+                <el-table border style="background:#EEF7FD;color:#20a0ff">
+                    <el-table-column label="序号" width="180"></el-table-column>
+                    <el-table-column label="调查名称" width="180"></el-table-column>
+                    <el-table-column label="调查内容" width="180"></el-table-column>
+                    <el-table-column label="考评对象" width="180"></el-table-column>
+                    <el-table-column label="调查时间" ></el-table-column>
+                </el-table>
+            </div>
         </div>
     </div>
 
